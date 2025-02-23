6edc26cc4f0d4ba8f68fe0d2ffa118d0ba9759e2
Vedant Lahane <59828215+MarkVed17@users.noreply.github.com>
2025-02-23 18:19:40 +0530

    chore: ignore eslint errors during build

--- next.config.js
+++ next.config.js
@@ -16,6 +16,9 @@ module.exports = withBundleAnalyzer({
   compiler: {
     styledComponents: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.svg$/,
